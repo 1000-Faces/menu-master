fc47c2dcbefc1472a2e560af929c144850b7a636
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-09-13 20:38:18 +0530

    Improved UI

--- Assets/Scripts/Meal/MealComponent.cs
+++ Assets/Scripts/Meal/MealComponent.cs
@@ -5,37 +5,58 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.AR;
 
+public class ComponentSelectionEventArgs : EventArgs
+{
+    public bool IsSelected { get; set; }
+}
+
 public class MealComponent : MonoBehaviour
 {
     [SerializeField] private MealComponentVisual foodCategoryVisualizer;
-    [SerializeField] private FoodCategory category;
+    private FoodCategory _category = FoodCategory.Unknown;
     // private ARSelectionInteractable arSelectionInteractable;
     // private ARPlacementInteractable arPlacementInteractable;
 
-    public static event EventHandler<ComponentInitializedEventArgs> OnComponentInitialized;
-    public class ComponentInitializedEventArgs : EventArgs
+    public static event EventHandler<ComponentSelectionEventArgs> OnComponentSelectionChanged;
+
+    public FoodCategory Category
     {
-        public MealComponent TargetComponent { get; set; }
+        get => _category;
+        set
+        {
+            _category = value;
+            ChangeCategory(_category);
+        }
     }
 
     private void Start()
     {
         // visualize the meal component
-        ChangeCategory(category);
+        ChangeCategory(_category);
 
         Utils.ShowToastMessage("Tap to change the category");
     }
 
-    public void OnSelectEntered(SelectEnterEventArgs arg0)
-    {
-        Utils.ShowToastMessage("Change the category");
-
-        // fire the event
-        OnComponentInitialized?.Invoke(this, new ComponentInitializedEventArgs { TargetComponent = this });
-    }
-
-    public void ChangeCategory(FoodCategory category)
+    private void ChangeCategory(FoodCategory category)
     {
         foodCategoryVisualizer.SwapObject(category);
+    }
+
+    public void OnSelectEntered(SelectEnterEventArgs arg0)
+    {
+        if (_category == FoodCategory.Unknown)
+        {
+            // Enable Add button using the event
+            OnComponentSelectionChanged?.Invoke(this, new ComponentSelectionEventArgs { IsSelected = true });
+        }
+    }
+
+    public void OnSelectExited(SelectExitEventArgs arg0)
+    {
+        if (_category == FoodCategory.Unknown)
+        {
+            // Enable Add button using the event
+            OnComponentSelectionChanged?.Invoke(this, new ComponentSelectionEventArgs { IsSelected = false });
+        }
     }
 }
